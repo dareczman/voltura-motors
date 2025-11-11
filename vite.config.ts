609e6303eb127fe8d5a3c2accc560a5a5eb95555
dar11335469 <dar11335469@gmail.com>
2025-11-11 11:49:58 +0100

    Add GitHub Pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/voltura-motors/",
   plugins: [vue(), vueDevTools()],
   css: {
     preprocessorOptions: {
@@ -16,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
