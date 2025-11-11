452c0e6cfde19987566a619c6c6f41b71cc8274d
dar11335469 <dar11335469@gmail.com>
2025-11-11 12:08:48 +0100

    Create base url and changed path to public directory

--- src/ui/UiGallery.vue
+++ src/ui/UiGallery.vue
@@ -1,49 +1,59 @@
 <script setup lang="ts">
-import { ref, watch, computed } from 'vue'
+import { ref, watch, computed } from "vue";
+import { getBaseUrl } from "@/utils/base-url.ts";
 
 interface Props {
-  gallery: string[]
+  gallery: string[];
 }
 
-const props = defineProps<Props>()
+const props = defineProps<Props>();
 
-const localGallery = ref<string[]>([...props.gallery])
-const mainImage = ref(localGallery.value[0] ?? '')
+const localGallery = ref<string[]>([...props.gallery]);
+const mainImage = ref(localGallery.value[0] ?? "");
 
 watch(
   () => props.gallery,
   (newGallery) => {
-    localGallery.value = [...newGallery]
-    mainImage.value = newGallery[0] ?? ''
+    localGallery.value = [...newGallery];
+    mainImage.value = newGallery[0] ?? "";
   },
   { immediate: true },
-)
+);
 
-const mainIndex = computed(() => localGallery.value.indexOf(mainImage.value))
+const mainIndex = computed(() => localGallery.value.indexOf(mainImage.value));
 
-const thumbnails = computed(() => localGallery.value.filter((_, idx) => idx !== mainIndex.value))
+const thumbnails = computed(() =>
+  localGallery.value.filter((_, idx) => idx !== mainIndex.value),
+);
 
 const swapImage = (image: string) => {
-  const clickedIndex = localGallery.value.indexOf(image)
-  const currentIndex = mainIndex.value
+  const clickedIndex = localGallery.value.indexOf(image);
+  const currentIndex = mainIndex.value;
 
-  if (clickedIndex === -1 || currentIndex === -1 || clickedIndex === currentIndex) return
+  if (
+    clickedIndex === -1 ||
+    currentIndex === -1 ||
+    clickedIndex === currentIndex
+  )
+    return;
 
-  const temp = localGallery.value[currentIndex]
-  localGallery.value[currentIndex] = localGallery.value[clickedIndex] as string
-  localGallery.value[clickedIndex] = temp as string
+  const temp = localGallery.value[currentIndex];
+  localGallery.value[currentIndex] = localGallery.value[clickedIndex] as string;
+  localGallery.value[clickedIndex] = temp as string;
 
-  mainImage.value = localGallery.value[currentIndex]
-}
+  mainImage.value = localGallery.value[currentIndex];
+};
 </script>
 
 <template>
   <div class="d-flex flex-column gap-3">
-    <div class="rounded-4 overflow-hidden position-relative main-image-wrapper w-100">
+    <div
+      class="rounded-4 overflow-hidden position-relative main-image-wrapper w-100"
+    >
       <Transition name="fade" mode="out-in">
         <img
           :key="mainImage"
-          :src="mainImage"
+          :src="getBaseUrl() + mainImage"
           alt="Zdjęcie samochodu"
           class="img-fluid w-100 h-100 position-absolute top-0 start-0 cursor-pointer"
         />
@@ -57,7 +67,11 @@ const swapImage = (image: string) => {
         class="thumbnail rounded-3 overflow-hidden border cursor-pointer h-auto ratio ratio-16x9"
         @click="swapImage(image)"
       >
-        <img :src="image" alt="Miniatura samochodu" class="w-100 h-100 object-fit-fill" />
+        <img
+          :src="getBaseUrl() + image"
+          alt="Miniatura samochodu"
+          class="w-100 h-100 object-fit-fill"
+        />
       </div>
     </div>
   </div>
